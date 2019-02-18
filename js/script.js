63a990d5b4433183b0a467a643a43a9bb668b2df
knight7024 <knight7024@naver.com>
2019-02-18 17:47:22 +0900

    Beautify Files

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var scrollTop = 0;
     $(window).scroll(function () {
         scrollTop = $(window).scrollTop();
-        scrollHeight = screen.availHeight - 200;
+        scrollHeight = screen.availHeight;
         // $('.counter').html(scrollTop);
 
         if (scrollTop >= scrollHeight) {
@@ -13,12 +13,12 @@ $(document).ready(function () {
         }
     });
 
-	$('.popup-video').magnificPopup({
-		disableOn: 700,
-		type: 'iframe',
-		mainClass: 'mfp-fade',
-		removalDelay: 10,
-		preloader: true,
-		fixedContentPos: false
-	});
+    $('.popup-video').magnificPopup({
+        disableOn: 700,
+        type: 'iframe',
+        mainClass: 'mfp-fade',
+        removalDelay: 10,
+        preloader: true,
+        fixedContentPos: false
+    });
 });
