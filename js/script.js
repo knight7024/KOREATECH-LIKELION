18a6ea9a9c62f7cfb537b1402759d63470dea7fb
knight7024 <knight7024@naver.com>
2019-02-20 05:48:28 +0900

    Few Things are Changed / Added

--- js/script.js
+++ js/script.js
@@ -1,17 +1,5 @@
 $(document).ready(function () {
     new WOW().init();
-    var scrollTop = 0;
-    $(window).scroll(function () {
-        scrollTop = $(window).scrollTop();
-        scrollHeight = screen.availHeight - 250;
-        // $('.counter').html(scrollTop);
-
-        if (scrollTop >= scrollHeight) {
-            $('#main-nav').addClass('scrolled-nav');
-        } else if (scrollTop < scrollHeight) {
-            $('#main-nav').removeClass('scrolled-nav');
-        }
-    });
 
     $('.popup-video').magnificPopup({
         disableOn: 700,
@@ -20,5 +8,27 @@ $(document).ready(function () {
         removalDelay: 10,
         preloader: true,
         fixedContentPos: false
+    });
+
+    $('#scroll-to-top').on('click', function () {
+        $('body, html').animate({
+            scrollTop: 0
+        }, 200);
+    });
+});
+
+$(window).on('scroll', function () {
+    var scrollTop = 0;
+    $(window).scroll(function () {
+        scrollTop = $(window).scrollTop();
+        scrollHeight = screen.availHeight - 250;
+
+        if (scrollTop >= scrollHeight) {
+            $('#main-nav').addClass('scrolled-nav');
+            $('#scroll-to-top').addClass('hideElement');
+        } else if (scrollTop < scrollHeight) {
+            $('#main-nav').removeClass('scrolled-nav');
+            $('#scroll-to-top').removeClass('hideElement');
+        }
     });
 });
